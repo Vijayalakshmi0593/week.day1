a610efe7d90f73ed92339b96dcf7440268c54614
Vijayalakshmi0593 <92051694+Vijayalakshmi0593@users.noreply.github.com>
2021-10-08 21:56:30 +0530

    Add files via upload

---
+++ LearnNegativetoPositive.java
@@ -0,0 +1,16 @@
+package week.day1;
+
+public class LearnNegativetoPositive {
+
+	public static void main(String[] args) {
+		 
+		int num = -40;
+		if( num<0)
+		{
+			System.out.println("The number is negative");
+			System.out.println("The number" + num + " is converted into " + (num * -1));
+		}
+
+	}
+
+}
